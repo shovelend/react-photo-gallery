d231ef9f44595f8c9fd1da2d0df631739cddd497
Dennis Ideler <ideler.dennis@gmail.com>
2017-10-02 21:45:06 +0100

    Specify property types for Image
    Typecheck the Image component's properties to reduce bugs.
    
    https://reactjs.org/docs/typechecking-with-proptypes.html

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './Image.css';
 
@@ -11,3 +12,9 @@ export default function Image({ src, alt, caption }) {
     </figure>
   );
 }
+
+Image.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string,
+  caption: PropTypes.string,
+};
